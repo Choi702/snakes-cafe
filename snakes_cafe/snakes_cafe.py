569709f8c0ae9727fda493de22b96a0183d4bb33
Choi702 <jaeychoi1@gmail.com>
2020-12-05 17:18:00 -0800

    first commit

---
+++ snakes_cafe/snakes_cafe.py
@@ -0,0 +1,20 @@
+
+
+
+print('Welcome to Snakes Cafe!')
+# wing_order = orders['wings']
+
+user_input = input('Please see our menu below.\n')
+
+user_input1 = ('To quit at any time, type "quit" ')
+
+# Appetizer = ["Wings", "Cookies", "Spring Rolls"]
+# Entrees = ["Salmon", "Steak", "Meat Tornado", "A Literal Garden"]
+# Desserts = ["Ice Cream", "Cake", "Pie"]
+# Drinks = ["Coffee", "Tea", "Unicorn Tears"]
+
+print(user_input)
+print(user_input1)
+# orders['wings'] += 1
+
+# print(orders)
