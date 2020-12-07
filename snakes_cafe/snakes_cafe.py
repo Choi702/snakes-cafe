0a374eecde37efa7d42145645e8ca483186648d4
Choi702 <jaeychoi1@gmail.com>
2020-12-06 17:18:11 -0800

    Cafe Working

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,26 +1,89 @@
-# list , dictionary
+print('**************************************')
+print('**    Welcome to the Snakes Cafe!   **')
+print('**    Please see our menu below.    **')
+print('**                                  **')
+print('** To quit at any time, type "quit" **')
+print('**************************************\n')
+print('Appetizers')
+print('----------')
+print('Wings')
+print('Cookies')
+print('Spring Rolls\n')
+print('Entrees')
+print('-------')
+print('Salmon')
+print('Steak')
+print('Meat Tornado')
+print('A Literal Garden\n')
+print('Desserts')
+print('--------')
+print('Ice Cream')
+print('Cake')
+print('Pie\n')
+print('Drinks')
+print('------')
+print('Coffee')
+print('Tea')
+print('Unicorn Tears\n')
+print('***********************************')
+print('** What would you like to order? **')
+print('***********************************')
 
-def snakes():
+# Dictonary
 
-    orders = {
-        'wings': 0,
-    }
+Orders = {
+  'Wings': 0,
+  'Cookies': 0,
+  'Spring Rolls': 0,
+  'Salmon': 0,
+  'Steak': 0,
+  'Meat Tornado': 0,
+  'A Litheral Garden': 0,
+  'Ice Cream': 0,
+  'Cake': 0,
+  'Pie': 0,
+  'Coffee': 0,
+  'Tea': 0,
+  'Unicorn Tears': 0
+}
+while True:
+    user_input = input('> ')
+    if user_input == "quit":
+      break
+    print(user_input)   
 
-    print(orders)
-
-    print('Welcome to Snakes Cafe')
-    wing_order = orders['wings']
-
-    user_input = input('What would you like to order?: ')
-
-    print(user_input)
-
-    print(orders['wings'])
-
-    orders['wings'] += 1
-
-    print(orders)
+    for key in Orders:
+      if user_input == key:
+        Orders[key] += 1
+        print(Orders[key])
+        print('** ' + str(Orders[key]) + ' Orders of ' + user_input + " have been added to your meal **")
 
 
-if __name__ == '__main__':
-    snakes()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
